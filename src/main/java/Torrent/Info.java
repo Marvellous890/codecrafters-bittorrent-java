492f7c57e9f4744f7b9703b3e72a2a843a91079c
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-10-16 12:40:22 +0100

    Tracker Get request

--- src/main/java/Torrent/Info.java
+++ src/main/java/Torrent/Info.java
@@ -2,31 +2,52 @@ package Torrent;
 
 public class Info {
     private String name;
+
     private long length;
-    private Piece[] pieces;
-    private String hash;
-    public String getHash() {
+
+    private int pieceLength;
+
+    private byte[][] pieces;
+
+    private byte[] hash;
+
+    public byte[] getHash() {
         return hash;
     }
-    public void setHash(String hash) {
+
+    public void setHash(byte[] hash) {
         this.hash = hash;
     }
+
     public long getLength() {
         return length;
     }
+
     public void setLength(long length) {
         this.length = length;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public Piece[] getPieces() {
+
+    public byte[][] getPieces() {
         return pieces;
     }
-    public void setPieces(Piece[] pieces) {
+
+    public void setPieces(byte[][] pieces) {
         this.pieces = pieces;
+    }
+
+    public int getPieceLength() {
+        return pieceLength;
+    }
+
+    public void setPieceLength(int pieceLength) {
+        this.pieceLength = pieceLength;
     }
 }
