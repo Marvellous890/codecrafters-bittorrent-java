26b62af16034f688b606954415daa7e0b5af2d4c
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-10-16 12:12:10 +0100

    pieces hashes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import B.Decoder;
+
 import Torrent.TorrentParser;
 import Torrent.Torrent;
+import Torrent.Piece;
+
 import com.google.gson.Gson;
 
 import java.io.File;
@@ -43,6 +46,15 @@ public class Main {
                 System.out.println("Tracker URL: " + torrent.getAnnounce());
                 System.out.println("Length: " + torrent.getInfo().getLength());
                 System.out.println("Info Hash: " + torrent.getInfo().getHash());
+
+                Piece[] pieces = torrent.getInfo().getPieces();
+
+                System.out.println("Piece Length: " + pieces[0].getLength());
+                System.out.println("Piece Hashes:");
+
+                for (Piece piece: pieces){
+                    System.out.println(piece.getHash());
+                }
             }
         } else {
             System.out.println("Unknown command: " + command);
